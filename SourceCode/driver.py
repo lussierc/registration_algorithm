8628d4fe9f1f1c787776a3b2fccfb803a804757a
clussier <lussierc@allegheny.edu>
2019-04-04 15:27:05 -0400

    Add some base code.

--- SourceCode/driver.py
+++ SourceCode/driver.py
@@ -1,2 +1,13 @@
 # Christian Lussier, Ben Watto, Mikey Spurr
 # This file will hold the driver code for the program
+
+def main():
+    """Driver program of the project."""
+    # Print welcome message:
+    print("*************************************************************")
+    print("* Thanks for using our Registration Letter Group Organizer! *")
+    print("*************************************************************")
+
+    file_name = input("Please enter a file name to use:   ")
+
+main()
--- SourceCode/organizer.py
+++ SourceCode/organizer.py
@@ -1,2 +1,5 @@
 # Christian Lussier, Ben Watto, Mikey Spurr
 # This file will hold the program's algorithm
+
+def letter_group_organizer():
+    """Letter Group Organizer Algorithm"""
