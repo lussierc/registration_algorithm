3dfd1d70fdf57a35336eac9bb7aded2a19e1348d
Mikey Spurr <spurrm@allegheny.edu>
2019-04-11 14:52:15 -0400

    file reader

---
+++ SourceCode/fileReader.py
@@ -0,0 +1,16 @@
+"""Process in the text file containing the students and their information regarding their registration"""
+# Christian Lussier, Ben Watto, Mikey Spurr
+# This work is ours unless otherwise cited.
+
+def registrationList_processor():
+    """Function to read in the student information"""
+    file_name = "../input/sample_students.txt" #finding the populated file
+
+    types = str, str, str, int, int # set the types of each item being read in
+
+    with open(file_name, "r") as file: # opening file to be parsed
+        stu_registationList = [] # create stu_registationList to store student data
+        for line in file: # parsing through the file
+            elements = tuple(t(e) for t,e in zip (types, line.split(";"))) #splitting up the data for student info, each students information is stored into a seperate tuple
+            stu_registationList.append(elements) # add tuples of song data to list
+    return stu_registationList
