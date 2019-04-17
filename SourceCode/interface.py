0762db5c487afbfda8f3409c49b4d683de37afc2
Mikey Spurr <spurrm@allegheny.edu>
2019-04-17 17:23:21 -0400

    adding in proper title names

--- SourceCode/interface.py
+++ SourceCode/interface.py
@@ -23,8 +23,8 @@ def main():
     output_file = open(chosen_file_name,'w') # opens previously created file, creates new file if necessary
     print() # print blank line for spacing
     print() # print blank line for spacing
-    print("{:<30}{:<25}{:<20}{:<15}{:<10}\n".format("Song Title", "Arist Name", "Genre", "Year", "Rank")) # print output header
-    output_file.write("{:<30}{:<25}{:<20}{:<15}{:<10}".format("Song Title", "Arist Name", "Genre", "Year", "Rank"))
+    print("{:<30}{:<25}{:<20}{:<15}{:<10}\n".format("Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank")) # print output header
+    output_file.write("{:<30}{:<25}{:<20}{:<15}{:<10}".format"Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank")
     for ele1,ele2,ele3,ele4,ele5 in sorted_students:
         print("{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
         output_file.write("\n{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
