f9ef445064f62d39cec8f1fcb1dc2f8f6a050eb1
Benjamin Watto <wattob@aldenv135.allegheny.edu>
2019-04-18 15:31:18 -0400

    Improving error handling for inputs

--- SourceCode/fileReader.py
+++ SourceCode/fileReader.py
@@ -6,10 +6,12 @@ def registrationList_processor(file_name):
     #file_name = "../input/sample_students.txt" #finding the populated file
 
     types = str, str, str, float, int # set the types of each item being read in
-
-    with open(file_name, "r") as file: # opening file to be parsed
-        stu_registationList = [] # create stu_registationList to store student data
-        for line in file: # parsing through the file
-            elements = tuple(t(e) for t,e in zip(types, line.split("; "))) # splitting up the data for student info, each students information is stored into a seperate tuple
-            stu_registationList.append(elements) # add tuples of song data to list
-    return(stu_registationList)
+    try:
+        with open(file_name, "r") as file: # opening file to be parsed
+            stu_registationList = [] # create stu_registationList to store student data
+            for line in file: # parsing through the file
+                elements = tuple(t(e) for t,e in zip(types, line.split("; "))) # splitting up the data for student info, each students information is stored into a seperate tuple
+                stu_registationList.append(elements) # add tuples of song data to list
+        return(stu_registationList)
+    except:
+        print("File inputting failed. Please enter a valid input file.")
--- SourceCode/interface.py
+++ SourceCode/interface.py
@@ -12,26 +12,29 @@ def main():
     print("*************************************************************")
     print()
 
-    try:
-        file_name = input("Sample input file example: ../input/sample_students.txt \n*** Please enter a file name to use:   ")
-    except:
-        print("Please enter a valid input file.")
+    file_name = input("Sample input file example: ../input/sample_students.txt \n*** Please enter a file name to use:   ")
+
     print()
 
     currentgroups = ["A", "B", "C", "F", "E", "D", "H", "G"]
-    student_list = registrationList_processor(file_name)
-    #print([x[2] for x in student_list])
+    try:
+        student_list = registrationList_processor(file_name)
+        #print([x[2] for x in student_list])
 
-    sorted_students = letter_group_organizer(currentgroups, student_list)
 
-    chosen_file_name = "../output/output.txt" # file name to output to
-    output_file = open(chosen_file_name,'w') # opens previously created file, creates new file if necessary
-    print() # print blank line for spacing
-    print() # print blank line for spacing
-    print("{:<30}{:<25}{:<20}{:<15}{:<10}\n".format("Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank")) # print output header
-    output_file.write("{:<30}{:<25}{:<20}{:<15}{:<10}".format("Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank"))
-    for ele1,ele2,ele3,ele4,ele5 in sorted_students:
-        print("{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
-        output_file.write("\n{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
-    output_file.close()
+        sorted_students = letter_group_organizer(currentgroups, student_list)
+
+
+        chosen_file_name = "../output/output.txt" # file name to output to
+        output_file = open(chosen_file_name,'w') # opens previously created file, creates new file if necessary
+        print() # print blank line for spacing
+        print() # print blank line for spacing
+        print("{:<30}{:<25}{:<20}{:<15}{:<10}\n".format("Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank")) # print output header
+        output_file.write("{:<30}{:<25}{:<20}{:<15}{:<10}".format("Student Name", "Class Year", "Letter Group", "GPA", "Prioity Rank"))
+        for ele1,ele2,ele3,ele4,ele5 in sorted_students:
+            print("{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
+            output_file.write("\n{:<30}{:<25}{:<20}{:<15}{:<10}".format(ele1,ele2,ele3,ele4,ele5))
+        output_file.close()
+    except:
+        print("\n Exiting....")
 main()
